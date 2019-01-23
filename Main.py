f88fa68e748772de9d7204422d2b8d34fd8588e7
Paweł <pawel89kaminski@gmail.com>
2019-01-23 19:35:16 +0100

    Final Fantasy :D

--- Main.py
+++ Main.py
@@ -1,12 +1,11 @@
 import NKJP as nkjp
 import Train as train
 
-
 wczytywanie_danych = False
 if wczytywanie_danych:
     # nkjp_dir = "C:\\Users\p.kaminski4\Desktop\INL_korpus_10_samples"
     nkjp_dir = "C:\\Users\Paweł\Documents\INL_korpus"
-    number_of_files = 10 # nie więcej niż 3889
+    number_of_files = 10      # nie więcej niż 3889
     nkjp.prepare_dictionary(nkjp_dir, number_of_files)
 if not wczytywanie_danych:
     daneU, nerU, daneR, nerR, daneT, nerT = train.load_data()
